b7cdaa4b4e1cea3c834019c136afa133c4fa2641
Ruyi Lan <rliu2400@gmail.com>
2025-05-25 00:53:26 -0400

    triv

---
+++ graph/dfs.h
@@ -0,0 +1,12 @@
+#include <vector>
+// returns yes if target can be reached
+bool dfs(int curr, int target, std::vector<bool> &visited, std::vector<std::vector<int>> &AL) {
+    if (curr == target)
+        return true;
+    visited[curr] = true;
+    for (int v : AL[curr])
+        if (!visited[v])
+            if (dfs(v, target, visited, AL))
+                return true;
+    return false;
+}
