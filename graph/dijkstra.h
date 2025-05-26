e86408815127b45dfc20cdfb9e2f1f8726ece015
Ruyi Lan <rliu2400@gmail.com>
2025-05-25 22:50:40 -0400

    polished

--- graph/dijkstra.h
+++ graph/dijkstra.h
@@ -2,21 +2,21 @@
 #include <utility>
 #include <vector>
 
-std::vector<int> shortest_path(int start, int N, std::vector<std::vector<std::pair<int, int>>> &AL) { // weight, node
+std::vector<int> shortest_path(int start, int N,
+                               const std::vector<std::vector<std::pair<int, int>>> &AL) { // weight, node
 
-    std::vector<int> dist(N, -1);
+    std::vector<int> dist(N, INT_MAX);
 
-    std::priority_queue<std::pair<int, int>> pq;
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
     pq.emplace(0, start);
-    while (pq.size()) {
-        auto &p = pq.top();
+    while (!pq.empty()) {
+        auto [w, u] = pq.top();
         pq.pop();
-        auto [w, u] = p;
         if (dist[u] != -1)
             continue;
-        dist[u] = -w;
-        for (std::pair<int, int> &p : AL[u]) {
-            pq.emplace(w - p.first, p.second);
+        dist[u] = w;
+        for (std::pair<int, int> p : AL[u]) {
+            pq.emplace(w + p.first, p.second);
         }
     }
 
